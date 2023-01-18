5744297c941bdf6912b41a2c4569444b92fe8203
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-01-18 16:22:40 -0700

    fix: 500/1000 event-queue split

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const COUNTLY_SETUP_DEFAULTS = {
   url: COUNTLY_API_URL,
   autoTrack: true,
   interval: 5000,
-  max_events: 1000,
+  max_events: 500,
   queue_size: 1000,
   session_update: 60
 }
