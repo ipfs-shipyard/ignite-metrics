8334d477ea5773b61c29fc034bce54d06c43250f
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-19 15:08:50 -0800

    chore: add StorageProvider class

---
+++ src/StorageProvider.ts
@@ -0,0 +1,20 @@
+import type { consentTypes } from './types/index.js'
+
+export interface StorageProviderInterface {
+  setStore: (values: consentTypes[]) => void
+  getStore: () => consentTypes[]
+}
+
+export class StorageProvider {
+  constructor(options: StorageProviderInterface) {
+    this.setStore = options.setStore ?? this.setStore
+    this.getStore = options.getStore ?? this.setStore
+  }
+
+  setStore(values: consentTypes[]): void {
+    throw new Error('Method not implemented')
+  }
+  getStore(): consentTypes[] {
+    throw new Error('Method not implemented')
+  }
+}
