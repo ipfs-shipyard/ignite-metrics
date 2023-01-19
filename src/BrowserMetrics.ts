0026429ae69344eed3999703ff3b233d9862847a
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-18 16:41:15 -0800

    fix: importing into starmap project

--- src/BrowserMetrics.ts
+++ src/BrowserMetrics.ts
@@ -2,8 +2,10 @@ import Countly from 'countly-sdk-web'
 
 import MetricsProvider, { MetricsProviderConstructorOptions } from './MetricsProvider.js'
 
-export default class BrowserMetricsProvider extends MetricsProvider<typeof Countly> {
+export class BrowserMetricsProvider extends MetricsProvider<typeof Countly> {
   constructor (args: Omit<MetricsProviderConstructorOptions<typeof Countly>, 'metricsService'>) {
     super({ ...args, metricsService: Countly })
   }
 }
+
+export default BrowserMetricsProvider
--- src/NodeMetrics.ts
+++ src/NodeMetrics.ts
@@ -2,8 +2,10 @@ import Countly from 'countly-sdk-nodejs'
 
 import MetricsProvider, { MetricsProviderConstructorOptions } from './MetricsProvider.js'
 
-export default class NodeMetricsProvider extends MetricsProvider<typeof Countly> {
+export class NodeMetricsProvider extends MetricsProvider<typeof Countly> {
   constructor (args: Omit<MetricsProviderConstructorOptions<typeof Countly>, 'metricsService'>) {
     super({ ...args, metricsService: Countly })
   }
 }
+
+export default NodeMetricsProvider
