1802c4e17acd1226839ed006b923f6c6baef2ce3
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2023-01-20 02:11:54 -0700

    breaking down logic

--- src/EventAccumulator.ts
+++ src/EventAccumulator.ts
@@ -35,38 +35,77 @@ export class EventAccumulator implements IEventAccumulator {
   }
 
   /**
+   * Pad events with default values
+   *
+   * @param {CountlyEvent} event - event to add defaults to
+   * @returns {CountlyEventData} - event with defaults added
+   */
+  private addEventDefaults (event: CountlyEvent): CountlyEventData {
+    return { ...eventDefaults, ...event }
+  }
+
+  /**
+   * Check if we're already accumulating an event by key.
+   *
+   * @param {string} key - event key
+   * @returns {boolean} - whether the event accumulator exists
+   */
+  private accumulatorExists (key: string): boolean {
+    return key in this.events
+  }
+
+  /**
+   * Setup the event accumulator for a key type for the first time.
+   *
+   * @param {CountlyEventData} eventData - event data
+   */
+  private setupEventAccumulator (eventData: CountlyEventData): void {
+    const { key } = eventData
+    this.events[key] = {
+      eventData,
+      // set start time to now. This will be updated when the event is flushed.
+      startTime: Date.now(),
+      // set a timeout to flush the event after the flush interval.
+      timeout: setTimeout(() => {
+        this.flush(key)
+      }, this.flushInterval)
+    }
+  }
+
+  /**
+   * Digest only the event data from an event.
+   *
+   * @param {CountlyEventData} newEventData - event data
+   */
+  private digestEventData (newEventData: CountlyEventData): void {
+    const { key, count, segmentation } = newEventData
+    // if event is in the store, update the event data.
+    const { eventData } = this.events[key]
+    eventData.count += count
+    eventData.sum += 1
+    eventData.segmentation = { ...eventData.segmentation, ...segmentation }
+  }
+
+  /**
    * Add an event to the accumulator
    *
    * @param {CountlyEvent} event - event to add
    * @param {boolean} flush - optionally whether to flush the event immediately
    */
   addEvent (event: CountlyEvent, flush: boolean = false): void {
-    // create a new event object with defaults.
-    const newEvent: CountlyEventData = { ...eventDefaults, ...event }
-    const { key, count } = newEvent
+    const eventData = this.addEventDefaults(event)
+    const { key } = eventData
 
     // validate event
     if (key === '') {
-      throw new Error('Event key is required')
+      throw new Error('Event key is required.')
     }
 
     // if event is not in the store, add it.
-    if (!(key in this.events)) {
-      this.events[key] = {
-        eventData: newEvent,
-        // set start time to now. This will be updated when the event is flushed.
-        startTime: Date.now(),
-        // set a timeout to flush the event after the flush interval.
-        timeout: setTimeout(() => {
-          this.flush(key)
-        }, this.flushInterval)
-      }
+    if (!this.accumulatorExists(key)) {
+      this.setupEventAccumulator(eventData)
     } else {
-      // if event is in the store, update the event data.
-      const { eventData } = this.events[key]
-      eventData.count += count
-      eventData.sum += 1
-      eventData.segmentation = { ...eventData.segmentation, ...newEvent.segmentation }
+      this.digestEventData(eventData)
     }
 
     // flush the event if flush is true.
