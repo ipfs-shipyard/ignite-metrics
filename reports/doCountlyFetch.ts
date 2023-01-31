2d8a907a08bb5c8d7d62abcc0a04459101405bf9
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-01-30 21:19:29 -0800

    Update reports/doCountlyFetch.ts
    Co-authored-by: Nishant Arora <1895906+whizzzkid@users.noreply.github.com>

--- reports/doCountlyFetch.ts
+++ reports/doCountlyFetch.ts
@@ -1,6 +1,16 @@
 import { apiKey, baseOptions, hostname } from './constants.js';
 
-export async function doCountlyFetch({ path = '/o', fetchOptions = {}, appId, extraParams }: { path: string, fetchOptions?: RequestInit, appId: string, extraParams: string }) {
+export async function doCountlyFetch({
+  appId,
+  extraParams,
+  fetchOptions = {},
+  path = '/o'
+}: {
+  appId: string,
+  extraParams: string,
+  fetchOptions?: RequestInit,
+  path: string
+}) {
   const response = await fetch(`https://${hostname}${path}?api_key=${apiKey}&app_id=${appId}&${extraParams}`, {...baseOptions, ...fetchOptions});
   return await response.json();
 }
